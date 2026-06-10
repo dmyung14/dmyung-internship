ebd49be896f037dc95ae166912329854c13f35d7
David Myung <djmyung07@gmail.com>
2026-06-10 14:28:07 -0400

    implemented new-items animations

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -2,8 +2,67 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import Item from "../UI/Item";
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import HotCollectionsNext from "./HotCollectionsNext";
+import HotCollectionsPrev from "./HotCollectionsPrev";
+import "../../css/styles/slider-arrows.css"
+import "../../css/styles/item-skeleton.css"
+import Skeleton from "../UI/Skeleton"
 
 const NewItems = () => {
+
+  const [items, setItems] = useState([])
+  const getItems = async () => {
+    await axios
+      .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems")
+      .then((response) => {
+        setItems(response.data)
+      })
+  }
+
+  let settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <HotCollectionsNext />,
+    prevArrow: <HotCollectionsPrev />,
+    centerMode: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+    ]
+  }
+
+  useEffect(() => {
+    getItems()
+  }, [])
+
+  const loading = items.length === 0
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +73,51 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
+          <Slider key={loading ? "loading" : "loaded"} {...settings}>
+            {loading 
+            ? new Array(4).fill(0).map((_, index) => (
+              <div key={index}>
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Skeleton 
+                      width="50px" 
+                      height="50px" 
+                      borderRadius="50%" />
                     <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
+                  <div className="nft__item_wrap" 
+                  style={{display: "flex", justifyContent: "center"}}>
+                    <Skeleton 
+                      width="100%" 
+                      height="200px" 
+                      style={{borderRadius: "8px"}}/>
+                  </div>
+                  <div className="nft__item_info">
+                    <Skeleton 
+                      width="60%" 
+                      height="20px" />
+                    <div style={{ marginTop: "10px" }}>
+                      <Skeleton 
+                        width="40%" 
+                        height="16px" />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            )) : items.map((item, index) => (
+                <Item
+                  key={index}
+                  authorId={item.authorId}
+                  authorImage={item.authorImage}
+                  nftImage={item.nftImage}
+                  nftId={item.nftId}
+                  title={item.title}
+                  price={item.price}
+                  likes={item.likes}
+                  expiryDate={item.expiryDate}
+                />
+              ))}
+          </Slider>
         </div>
       </div>
     </section>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -3,8 +3,12 @@ import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import { useParams } from 'react-router-dom'
 
 const Author = () => {
+
+  const { authorId } = useParams()
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
