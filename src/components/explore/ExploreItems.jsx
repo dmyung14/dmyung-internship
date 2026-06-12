b84404786e44f4eddace7ee788e591a0f48c5780
David Myung <djmyung07@gmail.com>
2026-06-12 05:20:32 -0400

    implemented author features

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -43,22 +43,17 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {loading ? new Array(8).fill(0).map((_, index) => (
-        <div className="nft__item" style={{width: "50%"}}>
-          <div className="author_list_pp">
-            <Skeleton width={"50px"} height={"50px"} borderRadius={"50%"} />
-            <i className="fa fa-check"></i>
-          </div>
-          <div className="nft__item_wrap" style={{display: "flex", justifyContent: "center"}}>
-            <Skeleton width={"100%"} height={"350px"} />
-          </div>
-          <div className="nft__item_info">
-            <Skeleton width={"180px"} height={"30px"} />
-            <Skeleton width={"100px"} height={"20px"} />
-            <Skeleton width={"30px"} height={"15px"} />
-          </div>
+      {loading ? 
+        <div className="row">
+          {new Array(8).fill(0).map((_, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+              <div className="nft__item">
+                <Skeleton width={"100%"} height={"400px"} />
+              </div>
+            </div>
+            ))}
         </div>
-      )) : items.slice(0, loadedItems).map((item, index) => (
+      : items.slice(0, loadedItems).map((item, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
