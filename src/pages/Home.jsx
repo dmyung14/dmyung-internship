3519909deff638c968bbfaea882ba5374e9f3a25
David Myung <djmyung07@gmail.com>
2026-06-12 05:55:24 -0400

    implemented david-animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,75 @@ import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: true,
+      offset: 100,
+    });
+
+    AOS.refresh();    
   }, []);
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
-        <Landing />
-        <LandingIntro />
-        <HotCollections />
-        <NewItems />
-        <TopSellers />
-        <BrowseByCategory />
+        
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-offset="0"
+        >
+          <Landing />
+        </div>
+
+
+        <div
+          data-aos="fade-down"
+          data-aos-duration="800"
+          data-aos-delay="100"
+        >
+          <LandingIntro />
+        </div>
+
+        <div
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="100"
+        >
+          <HotCollections />
+        </div>
+
+        <div
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="100"
+        >
+          <NewItems />
+        </div>
+
+        <div
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="100"
+        >
+          <TopSellers />
+        </div>
+
+        <div
+          data-aos="fade-left"
+          data-aos-duration="800"
+          data-aos-delay="100"
+        >
+          <BrowseByCategory />
+        </div>
       </div>
     </div>
   );
